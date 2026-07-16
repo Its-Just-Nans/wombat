9130cb2cc993b24a86df0f5211ee1251b785a1af
n4n5 <its.just.n4n5@gmail.com>
2026-07-16 20:34:18 +0200

    sync bladvak-cli

--- assets/gen_assets.sh
+++ assets/gen_assets.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-if [ ! -f "wombat.png" ]; then
-    convert -background none wombat.svg -resize 512x512 wombat.png
-fi
+APP_NAME="wombat"
 if [ ! -f "icon_ios_touch_192.png" ]; then
-    convert -background none wombat.svg -resize 192x192 icon_ios_touch_192.png
+    convert -background none "$APP_NAME.svg" -resize 192x192 icon_ios_touch_192.png
 fi
 if [ ! -f "icon-256.png" ]; then
-    convert -background none wombat.svg -resize 256x256 icon-256.png
+    convert -background none "$APP_NAME.svg" -resize 256x256 icon-256.png
 fi
 if [ ! -f "icon-1024.png" ]; then
-    convert -background none wombat.svg -resize 1024x1024 icon-1024.png
+    convert -background none "$APP_NAME.svg" -resize 1024x1024 icon-1024.png
 fi
 if [ ! -f "maskable_icon_x512.png" ]; then
-    convert -background none wombat.svg -resize 512x512 maskable_icon_x512.png
+    convert -background none "$APP_NAME.svg" -resize 512x512 maskable_icon_x512.png
 fi
 
 # https://golb.n4n5.dev/utils-linux.html#one-liner-faviconico-generator
 
-TO_ICONIFY=wombat.svg
+TO_ICONIFY="$APP_NAME.svg"
 if [ ! -f "favicon.ico" ]; then
     for i in 48 96 144 192; do convert -background none $TO_ICONIFY -resize ${i}x${i} favicon-${i}x${i}.png; done
     convert -background none favicon-* favicon.ico
@@ -26,7 +24,17 @@ if [ ! -f "favicon.ico" ]; then
 fi
 
 for i in *.svg; do
+    if [ "$i" = "$APP_NAME.svg" ]; then
+        continue
+    fi
     if [ ! -f "${i%.svg}.png" ]; then
         convert -background none "$i" -resize 64x64 "${i%.svg}.png"
     fi
 done
+
+if [ ! -f "$APP_NAME.png" ]; then
+    convert -background none "$APP_NAME.svg" -resize 512x512 "$APP_NAME.png"
+fi
+
+# The line beneath this is called `modeline`. See `:help modeline`
+# vim: ts=4 sts=4 sw=4 et
