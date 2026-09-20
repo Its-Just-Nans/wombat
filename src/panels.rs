012026a0ff4aeff507795ccff04c162a59b2dca0
n4n5 <its.just.n4n5@gmail.com>
2026-09-20 17:56:47 +0200

    add label

--- src/panels.rs
+++ src/panels.rs
@@ -96,21 +96,24 @@ impl BladvakPanel for FileInfo {
             &mut app.display_settings.show_color_picker,
             "Show color picker",
         );
-        egui::ComboBox::from_id_salt("data_view_combo_box")
-            .selected_text(app.display_settings.data_view.title())
-            .show_ui(ui, |ui| {
-                for one_view in DataView::all() {
+        ui.horizontal(|ui| {
+            ui.label("Default data view");
+            egui::ComboBox::from_id_salt("data_view_combo_box")
+                .selected_text(app.display_settings.data_view.title())
+                .show_ui(ui, |ui| {
+                    for one_view in DataView::all() {
+                        ui.selectable_value(
+                            &mut app.display_settings.data_view,
+                            one_view.clone(),
+                            one_view.title(),
+                        );
+                    }
                     ui.selectable_value(
                         &mut app.display_settings.data_view,
-                        one_view.clone(),
-                        one_view.title(),
+                        DataView::Empty,
+                        DataView::Empty.title(),
                     );
-                }
-                ui.selectable_value(
-                    &mut app.display_settings.data_view,
-                    DataView::Empty,
-                    DataView::Empty.title(),
-                );
-            });
+                });
+        });
     }
 }
